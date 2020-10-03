3af714327df7d9230f4e8cf5270e864838ba4b35
wish75584 <wish75584@gmail.com>
2020-10-03 09:33:08 +0530

    Initial commit code working design remian

---
+++ app/src/main/java/com/stubborn/fragmentexamplekotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.stubborn.fragmentexamplekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, BlankFragment()).commit()
+
+    }
+}
